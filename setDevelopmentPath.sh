1cc3f96f01712e31501d75367ff5d78ea4adbc42
bgschaid <bgschaid@e4e07f05-0c2f-0410-a05a-b8ba57e0c909>
2010-01-03 22:50:40 +0000

    Remove the hardcoded path from the script
    git-svn-id: svn://svn.code.sf.net/p/openfoam-extend/svn/trunk/Breeder/other/scripting/PyFoam@1565 e4e07f05-0c2f-0410-a05a-b8ba57e0c909

--- setDevelopmentPath.sh
+++ setDevelopmentPath.sh
@@ -1,7 +1,7 @@
 # points PATHs to the unstable development version
 # adapt to your own environment
 
-DEVELOPMENTDIR=/home/bgschaid/Development/OpenFOAM/Python/PyFoam
+DEVELOPMENTDIR=`dirname $0`
 
 export PATH=$DEVELOPMENTDIR/bin:$PATH
 export PYTHONPATH=$DEVELOPMENTDIR:$PYTHONPATH
