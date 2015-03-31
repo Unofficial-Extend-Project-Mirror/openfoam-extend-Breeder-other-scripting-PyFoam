eb1da33a9aed1eb4c596e2bd28e16f297d38a2b2
bgschaid <bgschaid@e4e07f05-0c2f-0410-a05a-b8ba57e0c909>
2015-03-31 23:48:39 +0000

    PyFoam is now on the Python Package index. Scripts were reworked to support this
    git-svn-id: svn://svn.code.sf.net/p/openfoam-extend/svn/trunk/Breeder/other/scripting/PyFoam@2065 e4e07f05-0c2f-0410-a05a-b8ba57e0c909

--- setup.py
+++ setup.py
@@ -1,16 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
 
 import glob,os,sys
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
 
 scriptlist =glob.glob(os.path.join('bin', '*.py'))
 scriptlist+=glob.glob(os.path.join('sbin', '*.py'))
 
 from PyFoam import versionString
-from PyFoam.ThirdParty.six import print_
 
-setup(name='PyFoam',
-      version=versionString(),
-      packages=['PyFoam',
+# with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+#    long_description = f.read()
+
+setup(
+    name='PyFoam',
+    version=versionString(),
+    packages=['PyFoam',
                 'PyFoam.Applications',
                 'PyFoam.Basics',
                 'PyFoam.Execution',
@@ -24,16 +32,29 @@ setup(name='PyFoam',
                 'PyFoam.ThirdParty.ply',
                 'PyFoam.ThirdParty.Gnuplot',
                 'PyFoam.Wrappers'],
-      description='Python Utilities for OpenFOAM',
-      url='http://www.ice-sf.at',
-      author='Bernhard Gschaider',
-      author_email='bgschaid@ice-sf.at',
-      scripts=scriptlist,
-      )
+    description='Python Utilities for OpenFOAM',
+    # long_description=long_description,
+    url='http://openfoamwiki.net/index.php/Contrib/PyFoam',
+    author='Bernhard F.W. Gschaider',
+    author_email='bgschaid@ice-sf.at',
+    scripts=scriptlist,
+    license="GPL",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
 
-try:
-    import numpy
-except ImportError:
-    print_("\n\n")
-    print_("numpy python-package not installed. Plotting won't work")
-    print_("\n\n")
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+    ],
+    keywords='cfd openfoam',
+
+    install_requires=['numpy'],
+)
